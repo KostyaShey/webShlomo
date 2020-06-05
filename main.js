a98d350eb958fba15b1bc509ce58f0ac8967ccf5
KostyaShey <sheynkler87@gmail.com>
2020-06-05 14:47:41 +0200

    added handlebars

--- main.js
+++ main.js
@@ -1,4 +1,26 @@
+const content = {
+    expences: [
+        { name: 'Sex', value: 69 },
+        { name: 'Drugs', value: 666 },
+        { name: "Rock'n'Roll", value: 11 },
+        { name: 'Yo Moma', value: 69 }],
+    income: [
+        { name: 'Hustle', value: 100 },
+        { name: 'Hustle', value: 100 },
+        { name: 'Hustle', value: 100 },
+        { name: 'Hustle', value: 100 },
+    ]
+}
+
+const siteContent = document.getElementById('contentScript');
+const siteContentScource = siteContent.innerHTML;
+const template = Handlebars.compile(siteContentScource);
+const compiledHtml = template(content);
+
+document.getElementById('content').innerHTML = compiledHtml;
+
+
 verticalRedLines = document.getElementById('redLines');
 list = document.getElementById('someList');
-lineHeight = list.offsetHeight;
+lineHeight = list.offsetHeight *2;
 verticalRedLines.style.height = lineHeight;
