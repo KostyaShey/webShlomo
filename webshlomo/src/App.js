929b4d9776cd81ae805aaf50982c9e52c33428ba
KostyaShey <sheynkler87@gmail.com>
2020-07-15 19:36:07 +0200

    rebuild webshlomo in react

--- webshlomo/src/App.js
+++ webshlomo/src/App.js
@@ -1,24 +1,80 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const expences = [
+    { name: 'Sex', value: 69 },
+    { name: 'Drugs', value: 666 },
+    { name: "Rock'n'Roll", value: 11 },
+    { name: 'Yo Moma', value: 69 }
+  ];
+
+  const income = [
+      { name: 'Hustle', value: 100 },
+      { name: 'Hustle', value: 100 },
+      { name: 'Hustle', value: 100 },
+      { name: 'Hustle', value: 100 },
+  ];
+
+  function buildRow (item) {
+    return (
+      <div className="row">
+        <div className="leftBorder"></div>
+        <div className="title">
+          <p>{item.name}</p>
+        </div>
+        <div className="value">
+          <p>{item.value} €</p>
+        </div>
+        <div className="button">
+          <p>button</p>
+        </div>
+      </div>
+    )
+  }
+
+  const expencesItems = expences.map(item => buildRow(item));
+  const incomeItems = income.map(item => buildRow(item));
+
+  function buildTitleRow(name) {
+    return (
+      <div className="row">
+          <div className="leftBorder"></div>
+          <div className="title bold">
+            <p>{name}</p>
+          </div>
+        </div>
+    )
+  }
+
+  function buildSumRow(data){
+    
+    let valueSum = data.reduce((currentSum, array) => currentSum + array.value, 0);
+    
+    return (
+      <div className="row">
+          <div className="leftBorder"></div>
+          <div className="title bold">
+            <p>Sum:</p>
+          </div>
+          <div className="value bold">
+            <p >{valueSum} €</p>
+          </div>
+        </div>
+    )
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="monthTable">
+        {buildTitleRow("Expences")}
+        {expencesItems}
+        {buildSumRow(expences)}
+        {buildTitleRow("Income")}
+        {incomeItems}
+        {buildSumRow(expences)}
+      </div>
     </div>
   );
 }
