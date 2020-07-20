efe2e57ff990862bf82d993ccb6e6cac31955b91
KostyaShey <sheynkler87@gmail.com>
2020-07-20 10:31:37 +0200

    stiling for the inputs

--- webshlomo/src/components/InputRow/InputRow.js
+++ webshlomo/src/components/InputRow/InputRow.js
@@ -6,20 +6,20 @@ export default class InputRow extends Component {
     render() {
 
             return (
-                <div className="row">
-                    <div className="leftBorder"></div>
-                    <form>
+                <form>
+                    <div className="row noHover ">
+                        <div className="leftBorder"></div>  
                         <div className="inputTitle">
-                            <input type="text" name="inputTitle" className="title"/>
+                            <input type="text" name="inputTitle" value="Title"/>
                         </div>
-                        <div>
-                            <input type="text" name="value" />
+                        <div className="inputValue">
+                            <input type="text" name="value" value="Value €"/>
+                            </div>
+                        <div className="inputButton">
+                            <input type="submit" style={{fontFamily: "FontAwesome"}} value="&#xf00c;" onClick={() => this.props.onClick('showAddRow')}/>
                         </div>
-                        <div>
-                            <input type="submit" value="Submit" className="button" onClick={() => this.props.onClick('showAddRow')}/>
-                        </div>
-                    </form>
-                </div>
+                    </div>
+                </form>
             )
     }
 }
