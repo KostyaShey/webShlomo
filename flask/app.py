64ed06402dff080028ff7f65be19cfc1769455d9
KostyaShey <sheynkler87@gmail.com>
2021-03-10 22:06:41 +0100

    making flask authentificate users with google

--- flask/app.py
+++ flask/app.py
@@ -7,11 +7,158 @@ from bson.objectid import ObjectId
 from flask import jsonify, request
 from werkzeug.security import generate_password_hash, check_password_hash
 
+#imports for oauth
+import json
+from flask import redirect, url_for
+from flask_login import (
+    LoginManager,
+    current_user,
+    login_required,
+    login_user,
+    logout_user,
+    UserMixin
+)
+from oauthlib.oauth2 import WebApplicationClient
+import requests
+
+#app setup
 app = Flask(__name__, static_folder='../webshlomo/build', static_url_path='/')
-app.secret_key = "kostya"
+app.secret_key = os.environ.get("SECRET_KEY", None)
 app.config['MONGO_URI'] = os.environ['MONGO_DB_LINK']
 mongo = PyMongo(app)
 
+#oauth config
+GOOGLE_CLIENT_ID = os.environ.get("GOOGLE_CLIENT_ID", None)
+GOOGLE_CLIENT_SECRET = os.environ.get("GOOGLE_CLIENT_SECRET", None)
+GOOGLE_DISCOVERY_URL = (
+    "https://accounts.google.com/.well-known/openid-configuration"
+)
+client = WebApplicationClient(GOOGLE_CLIENT_ID)
+# User session management setup
+# https://flask-login.readthedocs.io/en/latest
+login_manager = LoginManager()
+login_manager.init_app(app)
+# Flask-Login helper to retrieve a user from our db
+@login_manager.user_loader
+def load_user(user_id):
+    return User.get(user_id)
+
+#class for user management
+class User(UserMixin):
+    def __init__(self, id_, name, email):
+        self.id = id_
+        self.name = name
+        self.email = email
+
+    @staticmethod
+    def get(user_id):
+        user = mongo.db.users.find_one({"id_": user_id})
+        print(user)
+        print(type(user))
+        if user == None:
+            return None
+
+        user = User(
+            id_=user["id_"], name=user["name"], email=user["email"]
+        )
+        return user
+
+    @staticmethod
+    def create(id_, name, email):
+        mongo.db.users.insert(
+            {'id_': id_, 'name': name, 'email': email})
+
+
+def get_google_provider_cfg():
+    try:
+        return requests.get(GOOGLE_DISCOVERY_URL).json()
+    except Exception as e:
+        print(e)
+
+
+@app.route("/login")
+def login():
+    # Find out what URL to hit for Google login
+    google_provider_cfg = get_google_provider_cfg()
+    authorization_endpoint = google_provider_cfg["authorization_endpoint"]
+
+    # Use library to construct the request for Google login and provide
+    # scopes that let you retrieve user's profile from Google
+    request_uri = client.prepare_request_uri(
+        authorization_endpoint,
+        redirect_uri=request.base_url + "/callback",
+        scope=["openid", "email", "profile"],
+    )
+    return redirect(request_uri)
+
+@app.route("/login/callback")
+def callback():
+    # Get authorization code Google sent back to you
+    code = request.args.get("code")
+
+    google_provider_cfg = get_google_provider_cfg()
+    token_endpoint = google_provider_cfg["token_endpoint"]
+
+    # Prepare and send a request to get tokens! Yay tokens!
+    token_url, headers, body = client.prepare_token_request(
+        token_endpoint,
+        authorization_response=request.url,
+        redirect_url=request.base_url,
+        code=code
+    )
+    token_response = requests.post(
+        token_url,
+        headers=headers,
+        data=body,
+        auth=(GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET),
+    )
+
+    # Parse the tokens!
+    client.parse_request_body_response(json.dumps(token_response.json()))
+
+    # Now that you have tokens (yay) let's find and hit the URL
+    # from Google that gives you the user's profile information,
+    # including their Google profile image and email
+    userinfo_endpoint = google_provider_cfg["userinfo_endpoint"]
+    uri, headers, body = client.add_token(userinfo_endpoint)
+    userinfo_response = requests.get(uri, headers=headers, data=body)
+
+    # You want to make sure their email is verified.
+    # The user authenticated with Google, authorized your
+    # app, and now you've verified their email through Google!
+    if userinfo_response.json().get("email_verified"):
+        unique_id = userinfo_response.json()["sub"]
+        users_email = userinfo_response.json()["email"]
+        picture = userinfo_response.json()["picture"]
+        users_name = userinfo_response.json()["given_name"]
+    else:
+        return "User email not available or not verified by Google.", 400
+
+    # Create a user in your db with the information provided
+    # by Google
+    user = User(
+        id_=unique_id, name=users_name, email=users_email
+    )
+
+    # Doesn't exist? Add it to the database.
+    if User.get(unique_id) == None:
+        User.create(unique_id, users_name, users_email)
+
+    # Begin user session by logging the user in
+    login_user(user)
+
+    # Send user back to homepage
+    return redirect(url_for("index"))
+
+@app.route("/logout")
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for("index"))
+
+
+
+#helper function for response_builder function
 def pipeline_builder(type_of_data, month, year):
 
     if type_of_data == "expenses" or type_of_data  == "income":
@@ -28,6 +175,7 @@ def pipeline_builder(type_of_data, month, year):
 
     return pipeline
 
+#helper function for CRUD operations
 def response_builder(type_of_data, month, year):
     if type_of_data == "expenses" or type_of_data  == "income":
         data = mongo.db[type_of_data].find({"month": month, "year": year})
@@ -111,3 +259,6 @@ def update(type_of_data):
         _year = _new_data['year']
 
     return response_builder(type_of_data, _month, _year)
+
+if __name__ == "__main__":
+    app.run(ssl_context="adhoc")
